213015d364f176d190745c720de72edb6ae82805
jotape2112 <juanpablo.jorquera47@gmail.com>
2025-10-19 14:42:50 -0300

    Migrate user routes to ES6 module syntax

--- src/routes/userroutes.js
+++ src/routes/userroutes.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const { register, login, getProfile } = require('../controllers/userController');
-const auth = require('../middlewares/auth');
+import express from "express";
+import { register, login, getProfile } from "../controllers/userController.js";
+import auth from "../middlewares/auth.js";
 
-router.post('/register', register);
-router.post('/login', login);
-router.get('/profile', auth, getProfile);
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/profile", auth, getProfile);
 
 export default router;
