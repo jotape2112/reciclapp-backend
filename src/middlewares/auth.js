1dbfdd93bf04173ce8566da1a5a9b817026c623b
jotape2112 <juanpablo.jorquera47@gmail.com>
2025-10-19 14:54:28 -0300

    Refactor auth middleware to use ES6 imports

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,18 +1,19 @@
-const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
 
 const auth = (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
+
   if (!token) {
     return res.status(401).json({ message: "Acceso denegado, no hay token" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // guarda el id del usuario en req.user
+    req.user = decoded;
     next();
-  } catch (error) {
+  } catch (err) {
     res.status(400).json({ message: "Token inválido" });
   }
 };
 
-module.exports = auth;
+export default auth;
