c2db0c75ed7ef091694510b2536eed6e7f8008d1
jotape2112 <juanpablo.jorquera47@gmail.com>
2025-10-19 20:36:34 -0300

    Update request routes to include createRequest
    Refactor request routes to include createRequest and remove commented code.

--- src/routes/requestRoutes.js
+++ src/routes/requestRoutes.js
@@ -1,19 +1,17 @@
 import express from "express";
-import { getUserRequests, updateStatus, getAllRequests } from "../controllers/requestController.js";
+import {
+  createRequest,
+  getUserRequests,
+  getAllRequests,
+  updateStatus,
+} from "../controllers/requestController.js";
 import auth from "../middlewares/auth.js";
 
 const router = express.Router();
 
-// Usuario crea solicitud
-//router.post('/', auth, createRequest);
-
-// Usuario ve su historial
-router.get('/my', auth, getUserRequests);
-
-// Empresa/admin ve todas las solicitudes
-router.get('/', auth, getAllRequests);
-
-// Empresa actualiza estado (aceptar/rechazar/completar)
-router.put('/:id', auth, updateStatus);
+router.post("/", auth, createRequest);
+router.get("/my", auth, getUserRequests);
+router.get("/", auth, getAllRequests);
+router.put("/:id", auth, updateStatus);
 
 export default router;
