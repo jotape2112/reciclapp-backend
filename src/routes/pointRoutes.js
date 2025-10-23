7a00bad020d52585fca3addc215f0a396491a994
jotape2112 <juanpablo.jorquera47@gmail.com>
2025-10-23 19:52:37 -0300

    Update pointRoutes.js

--- src/routes/pointRoutes.js
+++ src/routes/pointRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getPoints, createPoint } from "../controllers/pointController.js";
-import { protect } from "../middleware/auth.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
