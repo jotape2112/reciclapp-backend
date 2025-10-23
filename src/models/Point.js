10286ab3678bcfe7d9be5ed4e81a7a87a9d3390b
jotape2112 <juanpablo.jorquera47@gmail.com>
2025-10-23 14:55:19 -0300

    Create Point.js

---
+++ src/models/Point.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const pointSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+    materials: [{ type: String }],
+    companyId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const Point = mongoose.model("Point", pointSchema);
+export default Point;
