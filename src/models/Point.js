f287d609f74ff3bddc0ef93c4684060f34b0c023
jotape2112 <juanpablo.jorquera47@gmail.com>
2025-10-30 04:08:15 -0300

    Update Point.js

--- src/models/Point.js
+++ src/models/Point.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-const pointSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    address: { type: String, required: true },
-    materials: { type: [String], required: true },
-    companyId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  },
-  { timestamps: true }
-);
+const pointSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
+  materials: [{ type: String }],
+  companyId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
 
 const Point = mongoose.model("Point", pointSchema);
 export default Point;
