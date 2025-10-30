1570328736fe2ffa10d263b347071d5e81c1b0d8
jotape2112 <juanpablo.jorquera47@gmail.com>
2025-10-30 00:36:27 -0300

    Update userroutes.js

--- src/routes/userroutes.js
+++ src/routes/userroutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { register, login, getProfile } from "../controllers/userController.js";
 import { protect } from "../middlewares/auth.js"; // ✅ Importa correctamente la función protect
+import { getCompanyStats } from "../controllers/requestController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/profile", protect, getProfile); // ✅ Usa protect en vez de auth
+router.get("/stats", protect, getCompanyStats);
 
 export default router;
