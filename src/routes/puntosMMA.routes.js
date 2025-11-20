bac43b584c335ee16b6cf78c1d4610a7f9425dbc
jotape2112 <juanpablo.jorquera47@gmail.com>
2025-11-20 20:23:09 -0300

    Add puntosMMA routes for fetching MMA points

---
+++ src/routes/puntosMMA.routes.js
@@ -0,0 +1,10 @@
+
+const express = require("express");
+const { getPuntosMMA } = require("../controllers/puntosMMA.controller");
+
+const router = express.Router();
+
+// GET /api/puntos-mma
+router.get("/", getPuntosMMA);
+
+module.exports = router;
